627114eda647d7149666124a9f69d951f596c0e5
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-12 17:07:14 -0400

    commitment

---
+++ src/ifelse.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ifelse {
+
+    public static void main (String[] args){
+    Scanner poop = new Scanner(System.in);
+
+
+
+    System.out.println("1: ");
+    int one = poop.nextInt();
+
+    System.out.println("2: ");
+    int two = poop.nextInt();
+
+    System.out.println("3: ");
+    int three = poop.nextInt();
+
+    System.out.println("4: ");
+    int four = poop.nextInt();
+
+    System.out.println("5: ");
+    int five = poop.nextInt();
+
+    System.out.println("6: ");
+    int six = poop.nextInt();
+
+    System.out.println("7: ");
+    int seven = poop.nextInt();
+
+    System.out.println("8: ");
+    int eight = poop.nextInt();
+
+    System.out.println("9: ");
+    int nine = poop.nextInt();
+
+    System.out.println("10: ");
+    int ten = poop.nextInt();
+
+    System.out.println((one + two + three + four + five + six + seven + eight + nine + ten)/10);
+
+    }
+}
